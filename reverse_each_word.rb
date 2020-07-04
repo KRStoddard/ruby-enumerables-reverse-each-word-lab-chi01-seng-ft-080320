e7d432a71fdc883fc363c1b8af0a10aeab2681dc
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-04 14:11:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
 firstsentence = sentence.split
-firstsentence.collect {|words| words.reverse}
-firstsentence.join(" ")
+newsentence = firstsentence.collect {|words| words.reverse}
+newsentence.join(" ")
 end 
